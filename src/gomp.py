3fc19f9ec92a479416efdcba24f90299ecb7115d
Anna Stawiska <ania.stawiska@gmail.com>
2023-03-28 17:13:32 +0200

    Updated convertong obstacles in GOMP class

--- src/gomp.py
+++ src/gomp.py
@@ -7,19 +7,33 @@ class GOMP:
 
     def convert_obstacles(self):
         obstacle_constraints = []
+        
+        minx = inf
+        maxx = -inf
+
+        miny = inf
+        maxy = -inf
+        
+        minz = inf
+        maxz = -inf
+
         for obstacle in self.obstacles:
-            obstacle_from_lines = obstacle.convert()
+            (obstacle_from_lines, x_constraints, y_constraints, z_constraints) = obstacle.convert()
             for line in obstacle_from_lines:
                 obstacle_constraints.append(line)
 
-        return obstacle_constraints
+            (minx, maxx) = (min(minx, x_constraints[0]), max(maxx, x_constraints[1]))
+            (miny, maxy) = (min(miny, y_constraints[0]), max(maxy, y_constraints[1]))
+            (minz, maxz) = (min(minz, z_constraints[0]), max(maxz, z_constraints[1]))
 
-    def run_solver(self):
+        return (obstacle_constraints, (minx, maxx), (miny, maxy), (minz, maxz))
+
+    def run_solver(self, line_constraints_c, x_constraints, y_constraints, z_constraints):
         pass
 
     def run(self):
-        line_constraints_c = self.convert_obstacles()
-        gomp_solutions = self.run_solver(line_constraints_c)
+        (line_constraints_c, x_constraints, y_constraints, z_constraints) = self.convert_obstacles()
+        gomp_solutions = self.run_solver(line_constraints_c, x_constraints, y_constraints, z_constraints)
 
         return gomp_solutions
 
