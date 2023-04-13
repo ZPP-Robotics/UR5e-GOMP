d3dd041f4bfe45e16162d5f39a932eb5c0a83cb2
Anna Stawiska <ania.stawiska@gmail.com>
2023-04-13 21:46:34 +0200

    Testing experiments on ursim

--- src/experiments/experiment_jointpos_to_jointpos.py
+++ src/experiments/experiment_jointpos_to_jointpos.py
@@ -1,18 +1,41 @@
 # This experiment will be done in a lab setting with two boxes.
 # We will get the starting position and end position as the 6 angles of the robot arm's joints.
 # We then will run our GOMP algorithm to get the best solution in the form of waypoints.
-
+import sys
+sys.path.append('../')
 from experiments_init import create_two_bin_lab_world, move_by_waypoints
 from gomp import GOMP
+import pandas as pd
+import rtde_control
 
 world_obstacles = create_two_bin_lab_world()
+(line, a, b) = world_obstacles.convert()
+print(line)
+print(a)
+print(b)
 
-gomp_solver = GOMP()
-for obstacle in world_obstacles:
-    gomp_solver.add_obstacle(obstacle)
+# gomp_solver = GOMP()
+# for obstacle in world_obstacles:
+#     gomp_solver.add_obstacle(obstacle)
 
-waypoints = gomp_solver.run()
+# waypoints = gomp_solver.run()
+df = pd.read_csv('output3.csv', delim_whitespace=True)
+path = df.values
+
+df['velo'] = 1
+df['acc'] = 1
+df['blend'] = 0.5
 
 # TODO - implement obtaining start pos
-start_pos = [0, 0, 0, 0, 0, 0]
-move_by_waypoints(start_pos=start_pos, waypoints=waypoints)
+# start_pos = [-7.143, -7.435, 7.20, -7.001, 7.12, 7.04]
+start_pos = [1.1109414100646973, -1.7925297222533167, -1.8433074951171875, -1.07748635232959, 1.5788192749023438, 2.88077712059021]
+
+print("connecting")
+
+ROBOT = rtde_control.RTDEControlInterface("10.0.0.219")
+
+ROBOT.moveJ(start_pos)
+print("start")
+ROBOT.moveJ(df.values[::3])
+print("done")
+# move_by_waypoints(start_pos=start_pos, waypoints=df.values)
--- src/experiments/experiments_init.py
+++ src/experiments/experiments_init.py
@@ -1,26 +1,36 @@
-from obstacles import Obstacle, BoxParallelXYZ, HorizontalLine
+import sys
+sys.path.append('../')
+
+from obstacles import Obstacle, BoxParallelXYZ, HorizontalLine , TwoBoxSetup
 import rtde_control
 
 def create_two_bin_lab_world():
-    box1 = BoxParallelXYZ((271.07, 507.62, 212.65), 51, 51, 25)
-    box2 = BoxParallelXYZ((271.07, 0.94, 212.65), 51, 51, 25)
+    # box1 = BoxParallelXYZ((271.07, 507.62, 212.65), 51, 51, 25)
+    # box2 = BoxParallelXYZ((271.07, 0.94, 212.65), 51, 51, 25)
 
-    return [box1, box2]
+    # return [box1, box2]
+    setup = TwoBoxSetup((271.07, 507.62, 212.65), 51, 51, 25,
+                        (271.07, 0.94, 212.65), 51, 51, 25)
+    return setup
+
+
 
 def move_by_waypoints(start_pos, 
                       waypoints,
-                      velocity = 0.5,
-                      acceleration = 0.5,
+                      velocity = 200,
+                      acceleration = 20000,
                       dt = 1.0/500,
                       lookahead_time = 0.1,
                       gain = 300):
     
-    rtde_c = rtde_control.RTDEControlInterface("127.0.0.1")
+    rtde_c = rtde_control.RTDEControlInterface("10.0.0.219")
 
     # Move to starting position
     rtde_c.moveJ(start_pos)
     for q_pos in waypoints:
+        t_start = rtde_c.initPeriod()
         rtde_c.servoJ(q_pos, velocity, acceleration, dt, lookahead_time, gain)
+        rtde_c.waitPeriod(t_start)
 
     rtde_c.servoStop()
     rtde_c.stopScript()
