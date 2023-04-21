737b5fa7992ca6c98cc78ddfbffc3e7f40ca9612
Anna Stawiska <ania.stawiska@gmail.com>
2023-04-21 13:50:44 +0200

    Two methods of running robot, moveJ and servoJ, - added one function to run the experiment

--- src/experiments/experiment_jointpos_to_jointpos.py
+++ src/experiments/experiment_jointpos_to_jointpos.py
@@ -4,7 +4,9 @@
 import sys
 sys.path.append('../')
 sys.path.append('../../OSQP-Solver/debug/tests/')
-from experiments_init import create_two_bin_lab_world, move_by_waypoints
+from colorama import Fore, Back, Style
+
+from experiments_init import create_two_bin_lab_world, move_by_waypoints, move_by_waypoints_moveJ
 from solver import GOMP
 import gomp
 
@@ -19,51 +21,70 @@ time_step = 0.1
 waypoints_count = 40 + 2
 
 position_constraints = ([Q_MIN] * N_DIM, [Q_MAX] * N_DIM)
-velocity_constraints = ([-M_PI] * N_DIM, [M_PI] * N_DIM)
+velocity_constraints = ([-M_PI / 10] * N_DIM, [M_PI / 10] * N_DIM)
 acceleration_constraints = ([-M_PI * 800 / 180] * N_DIM, [M_PI * 800 / 180] * N_DIM)
 #------------------------------------------------------------------------------------
+# radiuses = [0.05, 0.05, 0.05, 0.05, 0.15]
 
-
-# start_pos = [1.1109414100646973, -1.7925297222533167, -1.8433074951171875, -1.07748635232959, 1.5788192749023438, 2.88077712059021]
+# # start_pos = [1.1109414100646973, -1.7925297222533167, -1.8433074951171875, -1.07748635232959, 1.5788192749023438, 2.88077712059021]
 # start_pos = [36.88, -128.12, -97.43, -41.94, 87.60, 38.65]
 # for i in range(len(start_pos)):
 #     start_pos[i] = start_pos[i] * M_PI / 180
 # print(start_pos)
 
-start_pos = [1.1106168031692505, -1.9419242344298304, -1.959261417388916, -0.8121242088130494, 1.577633261680603, 2.8825175762176514]
-end_pos = [-0.16207582155336553, -2.0053416691222132, -1.8414853811264038, -0.8576411169818421, 1.5140776634216309, -0.08804780641664678]
-# end_pos = [-0.013442818318502248, -1.2449665826610108, -1.8534693717956543, -1.6163512669005335, 1.5796295404434204, 0.054004184901714325]
+# # start_pos = [1.1106168031692505, -1.9419242344298304, -1.959261417388916, -0.8121242088130494, 1.577633261680603, 2.8825175762176514]
+# end_pos = [-0.16207582155336553, -2.0053416691222132, -1.8414853811264038, -0.8576411169818421, 1.5140776634216309, -0.08804780641664678]
+# # end_pos = [-0.013442818318502248, -1.2449665826610108, -1.8534693717956543, -1.6163512669005335, 1.5796295404434204, 0.054004184901714325]
 
-world_obstacles = create_two_bin_lab_world()
 
-gomp_solver = GOMP(time_step, waypoints_count, position_constraints, velocity_constraints, acceleration_constraints)
-for obstacle in world_obstacles:
-    gomp_solver.add_obstacle(obstacle)
+# experiment(start_pos, end_pos, radiuses)
 
-print("Obstacles:")
-gomp_solver.print_obstacles()
+def experiment(start_pos, end_pos, radiuses):
+    world_obstacles = create_two_bin_lab_world()
 
-print("RUN")
-(code, waypoints) = gomp_solver.run(start_pos, end_pos, 0.1)
-waypoints = waypoints[:(len(waypoints)//2)]
+    gomp_solver = GOMP(time_step, waypoints_count, position_constraints, velocity_constraints, acceleration_constraints)
+    for obstacle in world_obstacles:
+        gomp_solver.add_obstacle(obstacle)
 
-if (code == gomp.OsqpExitCode.kOptimal):
-    print(len(waypoints[:(len(waypoints))])//6)
     print("Obstacles:")
     gomp_solver.print_obstacles()
 
-    path = []
-    for i in range(0, len(waypoints), 6):
-        next_joint_pos = waypoints[i:i+6]
-        path.append(next_joint_pos)
+    print("Radiuses:")
+    # radiuses = [0.05, 0.05, 0.05, 0.05, 0.15]
+    gomp_solver.set_radiuses(radiuses)
 
-    # next_joint_pos = waypoints[-6:-1]
-    # path.append(next_joint_pos)
-    for el in path:
-        print(el)
+    print("RUN")
+    (code, waypoints) = gomp_solver.run(start_pos, end_pos)
+    waypoints = waypoints[:(len(waypoints)//2)]
 
-    # move_by_waypoints(start_pos=start_pos, waypoints=path)
-else:
-    print("ERROR!")
-    print("No solution found")
-    print(code)
+    if (code == gomp.OsqpExitCode.kOptimal):
+        print(Fore.GREEN + Style.BRIGHT + "SUCCESS! - Found solution" + Style.RESET_ALL)
+        print(Style.BRIGHT + "Obstacles:" + Style.RESET_ALL)
+        gomp_solver.print_obstacles()
+
+
+        print(Style.BRIGHT + "Waypoint count:" + Style.RESET_ALL)
+        print(len(waypoints[:(len(waypoints))])//6)
+
+        # move_by_waypoints(start_pos=start_pos, waypoints=waypoints)
+        move_by_waypoints_moveJ(start_pos=start_pos, waypoints=waypoints)
+    else:
+        print(Fore.RED + Style.BRIGHT + "ERROR!" + Style.RESET_ALL)
+        print(Style.BRIGHT + "No solution found" + Style.RESET_ALL)
+        print(Style.BRIGHT + "Error code:" + Style.RESET_ALL)
+        print(code)
+
+# ------------------------------------------------------------------------------------
+radiuses = [0.1, 0.1, 0.05, 0.05, 0.15]
+
+# start_pos = [1.1109414100646973, -1.7925297222533167, -1.8433074951171875, -1.07748635232959, 1.5788192749023438, 2.88077712059021]
+start_pos = [36.88, -128.12, -97.43, -41.94, 87.60, 38.65]
+for i in range(len(start_pos)):
+    start_pos[i] = start_pos[i] * M_PI / 180
+print(start_pos)
+
+# start_pos = [1.1106168031692505, -1.9419242344298304, -1.959261417388916, -0.8121242088130494, 1.577633261680603, 2.8825175762176514]
+end_pos = [-0.16207582155336553, -2.0053416691222132, -1.8414853811264038, -0.8576411169818421, 1.5140776634216309, -0.08804780641664678]
+# end_pos = [-0.013442818318502248, -1.2449665826610108, -1.8534693717956543, -1.6163512669005335, 1.5796295404434204, 0.054004184901714325]
+
+experiment(start_pos, end_pos, radiuses)
--- src/experiments/experiments_init.py
+++ src/experiments/experiments_init.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append('../')
+from colorama import Fore, Back, Style
 
 from obstacles import TwoBoxSetup
 import rtde_control
 
 def create_two_bin_lab_world():
-    setup = TwoBoxSetup((271.07, 507.62, 212.65), 51, 51, 25,
-                        (271.07, 0.94, 212.65), 51, 51, 25)
+    setup = TwoBoxSetup((0.27107, 0.50762, 0.21265), 0.51, 0.51, 0.25,
+                        (0.27107, 0.094, 0.21265), 0.51, 0.51, 0.25)
     return [setup]
 
 def move_by_waypoints(start_pos, 
@@ -16,47 +17,61 @@ def move_by_waypoints(start_pos,
                       dt = 1.0/500,
                       lookahead_time = 0.1,
                       gain = 300):
+    path = []
+    for i in range(0, len(waypoints), 6):
+        next_joint_pos = waypoints[i:i+6]
+        path.append(next_joint_pos)
+
+    print(Style.BRIGHT + "Waypoints:" + Style.RESET_ALL)
+    for el in path:
+        print(el)
+
     print("connecting to robot")
-    rtde_c = rtde_control.RTDEControlInterface("10.0.0.219")
+    robot_ip = "192.168.1.20"
+    # rtde_c = rtde_control.RTDEControlInterface("10.0.0.219")
+    # rtde_c = rtde_control.RTDEControlInterface(robot_ip)
 
     # Move to starting position
-    print("moving to start position")
-    rtde_c.moveJ(start_pos)
-    print("starting movement")
-    for q_pos in waypoints:
-        t_start = rtde_c.initPeriod()
-        rtde_c.servoJ(q_pos, velocity, acceleration, dt, lookahead_time, gain)
-        rtde_c.waitPeriod(t_start)
+    # print("moving to start position")
+    # rtde_c.moveJ(start_pos)
+    # print("starting movement")
+    # for q_pos in waypoints:
+    #     t_start = rtde_c.initPeriod()
+    #     rtde_c.servoJ(q_pos, velocity, acceleration, dt, lookahead_time, gain)
+    #     rtde_c.waitPeriod(t_start)
 
     # rtde_c.servoStop()
     # rtde_c.stopScript()
     print("end")
 
 def move_by_waypoints_moveJ(waypoints, start_pos):
+    robot_ip = "192.168.1.20"
     velo = 1
     acc = 1
     blend = 0.1
 
     path = []
     for i in range(0, len(waypoints), 6):
+        next_joint_pos = []
         next_joint_pos = waypoints[i:i+6]
         next_joint_pos.append(velo)
         next_joint_pos.append(acc)
         next_joint_pos.append(blend)
+        print(next_joint_pos)
         path.append(next_joint_pos)
 
-    next_joint_pos = waypoints[-6:-1]
-    next_joint_pos.append(velo)
-    next_joint_pos.append(acc)
-    next_joint_pos.append(blend)
-    path.append(next_joint_pos)
+    # next_joint_pos = waypoints[-6:-1]
+    # next_joint_pos.append(velo)
+    # next_joint_pos.append(acc)
+    # next_joint_pos.append(blend)
+    # path.append(next_joint_pos)
 
-    print(path[::3])
+    # print(path[::3])
 
-    print("connecting")
-    ROBOT = rtde_control.RTDEControlInterface("10.0.0.219")
+    # print("connecting")
+    # ROBOT = rtde_control.RTDEControlInterface(robot_ip)
 
-    ROBOT.moveJ(start_pos)
-    print("start")
-    ROBOT.moveJ(path[::3])
-    print("done")
+    # ROBOT.moveJ(start_pos)
+    # print("start")
+    # ROBOT.moveJ(path[::3])
+    # print("done")
