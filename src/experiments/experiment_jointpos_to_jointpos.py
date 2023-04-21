745e912c580d016848ba94ba9030ef5d36eac5ec
Anna Stawiska <ania.stawiska@gmail.com>
2023-04-21 11:27:41 +0200

    Take into account in experiment the error code returned by solver

--- src/experiments/experiment_jointpos_to_jointpos.py
+++ src/experiments/experiment_jointpos_to_jointpos.py
@@ -6,6 +6,7 @@ sys.path.append('../')
 sys.path.append('../../OSQP-Solver/debug/tests/')
 from experiments_init import create_two_bin_lab_world, move_by_waypoints
 from solver import GOMP
+import gomp
 
 # INITIAL SETUP ---------------------------------------------------------------------
 N_DIM = 6
@@ -23,8 +24,15 @@ acceleration_constraints = ([-M_PI * 800 / 180] * N_DIM, [M_PI * 800 / 180] * N_
 #------------------------------------------------------------------------------------
 
 
-start_pos = [1.1109414100646973, -1.7925297222533167, -1.8433074951171875, -1.07748635232959, 1.5788192749023438, 2.88077712059021]
-end_pos = [-0.013442818318502248, -1.2449665826610108, -1.8534693717956543, -1.6163512669005335, 1.5796295404434204, 0.054004184901714325]
+# start_pos = [1.1109414100646973, -1.7925297222533167, -1.8433074951171875, -1.07748635232959, 1.5788192749023438, 2.88077712059021]
+# start_pos = [36.88, -128.12, -97.43, -41.94, 87.60, 38.65]
+# for i in range(len(start_pos)):
+#     start_pos[i] = start_pos[i] * M_PI / 180
+# print(start_pos)
+
+start_pos = [1.1106168031692505, -1.9419242344298304, -1.959261417388916, -0.8121242088130494, 1.577633261680603, 2.8825175762176514]
+end_pos = [-0.16207582155336553, -2.0053416691222132, -1.8414853811264038, -0.8576411169818421, 1.5140776634216309, -0.08804780641664678]
+# end_pos = [-0.013442818318502248, -1.2449665826610108, -1.8534693717956543, -1.6163512669005335, 1.5796295404434204, 0.054004184901714325]
 
 world_obstacles = create_two_bin_lab_world()
 
@@ -36,17 +44,26 @@ print("Obstacles:")
 gomp_solver.print_obstacles()
 
 print("RUN")
-waypoints = gomp_solver.run(start_pos, end_pos)
+(code, waypoints) = gomp_solver.run(start_pos, end_pos, 0.1)
 waypoints = waypoints[:(len(waypoints)//2)]
 
-print(len(waypoints[:(len(waypoints)//2)])//6)
+if (code == gomp.OsqpExitCode.kOptimal):
+    print(len(waypoints[:(len(waypoints))])//6)
+    print("Obstacles:")
+    gomp_solver.print_obstacles()
 
-path = []
-for i in range(0, len(waypoints), 6):
-    next_joint_pos = waypoints[i:i+6]
-    path.append(next_joint_pos)
+    path = []
+    for i in range(0, len(waypoints), 6):
+        next_joint_pos = waypoints[i:i+6]
+        path.append(next_joint_pos)
 
-next_joint_pos = waypoints[-6:-1]
-path.append(next_joint_pos)
+    # next_joint_pos = waypoints[-6:-1]
+    # path.append(next_joint_pos)
+    for el in path:
+        print(el)
 
-move_by_waypoints(start_pos=start_pos, waypoints=path)
+    # move_by_waypoints(start_pos=start_pos, waypoints=path)
+else:
+    print("ERROR!")
+    print("No solution found")
+    print(code)
